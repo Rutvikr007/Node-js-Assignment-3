24011f2ee5d762bec72bd01bb28be2112c6563d5
RUTVIK  RAMTEKE <95290638+Rutvikr007@users.noreply.github.com>
2022-07-05 21:09:55 +0530

    Add files via upload

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "nodejs-assignment-p3",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "dev": "nodemon index.js"
+  },
+  "keywords": [],
+  "author": "Rutvik Ramteke",
+  "license": "MIT"
+}
